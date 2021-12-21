50ff9fa6b57a3dc1a2b7488c2e16e17e7a0f1a6b
Anyaorah-Emmanuel <tanyaorah@gmail.com>
2021-12-21 22:30:16 +0100

    Rename view_display to see_display
    rename view_display on lines 75 and 76 of the bikeshare.py file to see_display

--- bikeshare.py
+++ bikeshare.py
@@ -72,8 +72,8 @@ def load_data(city, month, day):
         if view_data == "yes":
             print(df.iloc[start_loc:start_loc + 5])
             start_loc += 5
-        view_display = input("Do you wish to continue?: Enter yes or no\n").lower()
-        if view_display != "yes":
+        see_display = input("Do you wish to continue?: Enter yes or no\n").lower()
+        if see_display != "yes":
             break
 # convert the Start Time column to datetime 
     df['Start Time'] = pd.to_datetime(df['Start Time']) 
